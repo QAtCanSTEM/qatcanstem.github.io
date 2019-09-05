0b6dfba44a21ce456708768a34aad66344e5ef51
Landon Getz <landon.getz@dal.c>
2019-09-05 09:50:17 -0300

    Fixed profile image on Index
    Made a typo, fixed it to fix photo on main page.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Name: Landon Getz How do you Identify: Queer and Gay Website: http://www.landongetz.com Twitter: @landongetz Current Job: PhD Candidate Scientific Field/Discipline: Microbiology (Bacteriology specifically) Affiliation: Dalhousie University Tell Us about yourself: I am a Queer and Gay PhD Student studying at Dalhousie University. I am an LGBTQ+ Advocate and an amateur...","categories": [],
         "tags": [],
         "url": "https://qatcanstem.github.io/LandonGetz/",
-        "teaser":"https://qatcanstem.github.io/assets/images/Profiles/LGteaser.jpg"}]
+        "teaser":"https://qatcanstem.github.io/assets/images/Profiles/LGteaser.jpg"},{
+        "title": "Queer Atlantic Canadian STEMist - Rachel Clarke",
+        "excerpt":"Name: Rachel Clarke How do you Identify: Queer and bisexual Twitter: @RachelGClarke Current Job: MSc. candidate Scientific Field/Discipline: Mercury photochemistry Affiliation: Acadia University Tell Us about yourself: I study at Acadia University, I’m also queer and bisexual. I am an eclectic environmentalist, a novice knitter, a devoted cat-Mom, and an...","categories": [],
+        "tags": [],
+        "url": "https://qatcanstem.github.io/RachelClarke/",
+        "teaser":"https://qatcanstem.github.io/assets/images/Profiles/RC.jpg"}]
